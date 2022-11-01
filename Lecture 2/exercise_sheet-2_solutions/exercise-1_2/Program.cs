7225fbbbd1695b608c3ac68e31891000fa3000b0
korayinal <korayinal93@gmail.com>
2022-11-01 11:38:48 +0100

    Exercise Solutions

---
+++ Lecture 2/exercise_sheet-2_solutions/exercise-1_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PSWE
+{
+    class Lecture2
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a complex number: ");
+            var x = Console.ReadLine();
+
+            Complex c;
+
+            bool works = Complex.TryParseComplex(x, out c);
+
+            if (works) // checks if parsing worked or not
+            {
+                Console.WriteLine("Parsed successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Parsing failed. Invalid complex number.");
+            }
+            Console.WriteLine("Norm: " + c.GetNorm());
+            
+        }
+    } // Lecture2
+} //PSWE
---
+++ Lecture 2/exercise_sheet-2_solutions/exercise-3.cs
@@ -0,0 +1,33 @@
+﻿/* This program returns factorial of a non-negative number.
+        * It takes user input from console, checks if it is non-negative.
+        * Compute: n! = n*(n-1)! = n*(n-1)*...*3*2*1 (recursively)
+        * Special case: 0! = 1  */
+using System;
+namespace Lecture2
+{
+    class FactorialExercise
+    {
+        // a function named 'Factorial' is defined which takes one integer-type input and returns an integer-type value
+        public static int Factorial(int number) // 'static' so that a class object isn't required when calling this
+        {
+            if (number == 0)
+                return 1; // 0! = 1
+            else
+                return number * Factorial(number - 1); // n! = n*(n-1)!
+        }
+        public static void Main()
+        {
+            Console.Write("Enter a number: ");
+            int inputNumber = Convert.ToInt32(Console.ReadLine()); // reading and converting user input
+           
+            while(inputNumber < 0) // check for negative numbers
+            {
+                Console.Write("Number shouldn't be negative.\nEnter a number: ");
+                inputNumber = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int result = FactorialExercise.Factorial(inputNumber); // function call to 'Factorial' defined above 
+            Console.WriteLine(inputNumber + "! is " + result);
+        }
+    } // FactorialExercise
+} // Lecture2
