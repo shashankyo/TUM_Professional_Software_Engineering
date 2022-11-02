052c7b4613793031636bce56ea3752d8c37a5828
korayinal <korayinal93@gmail.com>
2022-11-02 20:36:44 +0100

    Assignment 1 - first try

--- Lecture 2/Assignment_1/Assignment_1/Geometry.cs
+++ Lecture 2/Assignment_1/Assignment_1/Geometry.cs
@@ -1,42 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Assignment_1
+namespace ProSE_Assignment1
 {
     public abstract class ImplicitGeometry
     {
-    public abstract bool IsInside(double x, double y);
+        public abstract bool IsInside(double x, double y);
 
         public void Visualize(double xGrid, double yGrid)
         {
-            Console.WriteLine("Visualized");
-        }
+            // char gridChar = '.';
+            for (double y = yGrid - 1; y >= 0; y--)
+            {
 
+                for (int x = 0; x < xGrid; x++)
+                {
+                    Console.Write($"({x},{y})");
+                }
+                Console.Write('\n');
+            }
+        }
     }
-    public class Circle : ImplicitGeometry
+    //public class Circle : ImplicitGeometry
+    //{
+    //    public double xCenter;
+    //    public double yCenter;
+    //    public double radius;
+    //    public override bool IsInside(double x, double y)
+    //    {
+    //        return Math.Sqrt(x * x + y * y);
+    //    }
+    //    public Circle(double xCenter, double yCenter, double radius)
+    //    {
+    //        this.xCenter = xCenter;
+    //        this.yCenter = yCenter;
+    //        this.radius = radius;
+    //    }
+    //}
+    public class Rectangle : ImplicitGeometry
     {
+        public double x1;
+        public double x2;
+        public double y1;
+        public double y2;
+
+
+        public Rectangle(double x1, double x2, double y1, double y2)
+        {
+            this.x1 = x1;
+            this.x2 = x2;
+            this.y1 = y1;
+            this.y2 = y2;
+        }
         public override bool IsInside(double x, double y)
         {
-            return;
-        }
-        public double x;
-        public double y;
-        public double radius;
-        public Circle(double x, double y, double radius)
-        {
-            this.x = x;
-            this.y = y;
-            this.radius = radius;
-            Console.WriteLine("Circle has been made.");
-
-        }
-
-       
-
+         Math.
     }
+    //public abstract class Operation : ImplicitGeometry
+    //{
+    //    public abstract ImplicitGeometry Operation();
+
+    //}
+}
 }
 
 
--- Lecture 2/Assignment_1/Assignment_1/Program.cs
+++ Lecture 2/Assignment_1/Assignment_1/Program.cs
@@ -1,14 +1,26 @@
-﻿using Assignment_1;
-using System;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ProSE_Assignment1
 {
     class Program
     {
+        // public const int sizeGridX = 5;
+        // public const int sizeGridY = 5;
+        // public const char gridChar= '.';
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hey!");
-            Circle c1 = new Circle(0, 0, 2);
-            c1.Visualize(3, 4);
+            Rectangle rec1 = new Rectangle(2,5,2,5);
+            rec1.Visualize(9,9);
+
         }
+
+
     }
 }
