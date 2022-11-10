b2dc2f7157313379f1aa1e588da8de678993291e
ge85git <ge85git@mytum.de>
2022-11-10 15:34:42 +0100

    Assignment 1 - fine tuning

--- Lecture 2/Assignment_1/Assignment_1/Geometry.cs
+++ Lecture 2/Assignment_1/Assignment_1/Geometry.cs
@@ -13,55 +13,23 @@ namespace ProSE_Assignment1
 {
     public abstract class ImplicitGeometry
     {
-
         public abstract bool IsInside(ref double x, ref double y);
-        public abstract bool IsInside(ref double y);
         public void Visualize(double xGrid, double yGrid)
         {
             char falseGridChar = ' ';
             char trueGridChar = '#';
             bool isInside = false;
-            // IsInside(xGrid, yGrid);
-            //
-            //
-            // BUG!!
-            // tam olarak çalışmıyor, xGrid'e IsInside'ın true olduğu aralıktan büyük bir değer verdiğimde, hep false dönüyor
-            // 
-            //
             for (double y = yGrid; y >= 0; y--)
             {
-                isInside = IsInside(ref y);
-                if (isInside == true)
-                {
                     for (double x = 0; x <= xGrid; x++)
                     {
                         isInside = IsInside(ref x, ref y);
                         if (isInside == true)
                         {
-                            Console.Write($"({x},{y})");
-                            //Console.BackgroundColor = ConsoleColor.Blue;
-                            //Console.ForegroundColor = ConsoleColor.Blue;
-                            //Console.Write(trueGridChar);
-                        }
-                        else
-                        {
                             //Console.Write($"({x},{y})");
-                            //Console.BackgroundColor = ConsoleColor.White;
-                            Console.Write(falseGridChar);
-                        }
-                    }
-                }
-                else
-                {
-                    for (int x = 0; x <= xGrid; x++)
-                    {
-                        if (isInside == true)
-                        {
                             //Console.BackgroundColor = ConsoleColor.Blue;
                             //Console.ForegroundColor = ConsoleColor.Blue;
                             Console.Write(trueGridChar);
-                            //Console.BackgroundColor = ConsoleColor.White;
-                            isInside = false;
                         }
                         else
                         {
@@ -70,7 +38,6 @@ namespace ProSE_Assignment1
                             Console.Write(falseGridChar);
                         }
                     }
-                }
                 //Console.BackgroundColor = ConsoleColor.White;
                 Console.Write('\n');
             }
@@ -82,8 +49,6 @@ namespace ProSE_Assignment1
         public double x2;
         public double y1;
         public double y2;
-
-
         public Rectangle(double x1, double x2, double y1, double y2)
         {
             this.x1 = x1;
@@ -109,18 +74,6 @@ namespace ProSE_Assignment1
                 return false;
             }
         }
-        public override bool IsInside(ref double y)
-        {
-            if (y >= y1 && y <= y2)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-
-        }
     }
 
     public class Circle : ImplicitGeometry
@@ -135,15 +88,10 @@ namespace ProSE_Assignment1
             this.yCenter = yCenter;
             this.radius = radius;
         }
-        public override bool IsInside(ref double y)
-        {
-
-                return true;
-            
-        }
         public override bool IsInside(ref double x, ref double y)
         {
-            if ((x - center_x) ^ 2 + (y - center_y) ^ 2 < radius ^ 2)
+            if ((x - xCenter) * (x - xCenter) +
+            (y - yCenter) * (y - yCenter) <= radius * radius)
             {
                 return true;
             }
@@ -153,19 +101,27 @@ namespace ProSE_Assignment1
             }
         }
     }
-
     public abstract class Operation : ImplicitGeometry
     {
         public Operation(ref Rectangle operand1, ref Rectangle operand2)
         {
         }
-        //public Operation(ref Circle operand1, ref Circle operand2)
-        //{
-        //}
-        //public Operation(ref Circle operand1, ref Rectangle operand2)
-        //{
-        //}
+        public Operation(ref Circle operand1, ref Circle operand2)
+        {
+        }
+        public Operation(ref Circle operand1, ref Rectangle operand2)
+        {
+        }
         public Operation(ref Union operand1, ref Union operand2)
+        {
+        }
+        public Operation(ref Difference operand1, ref Difference operand2)
+        {
+        }
+        public Operation(ref Union operand1, ref Difference operand2)
+        {
+        }
+        public Operation(ref Rectangle operand1, ref Difference operand2)
         {
         }
 
@@ -179,22 +135,25 @@ public class Union : Operation
             this.operand1 = operand1;
             this.operand2 = operand2;
         }
-        //public Union(ref Circle operand1, ref Circle operand2) : base(ref operand1, ref operand2)
-        //{
-        //    this.operand1 = operand1;
-        //    this.operand2 = operand2;
-        //}
+        public Union(ref Circle operand1, ref Circle operand2) : base(ref operand1, ref operand2)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
         public Union(ref Union operand1, ref Union operand2) : base(ref operand1, ref operand2)
         {
             this.operand1 = operand1;
             this.operand2 = operand2;
         }
-        public override bool IsInside(ref double y)
+        public Union(ref Difference operand1, ref Difference operand2) : base(ref operand1, ref operand2)
         {
-            
-            bool insideOperand1 = operand1.IsInside(ref y);
-            bool insideOperand2 = operand2.IsInside(ref y);
-            return insideOperand1 || insideOperand2;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
+        }
+        public Union(ref Union operand1, ref Difference operand2) : base(ref operand1, ref operand2)
+        {
+            this.operand1 = operand1;
+            this.operand2 = operand2;
         }
         public override bool IsInside(ref double x, ref double y)
         {
@@ -212,19 +171,16 @@ public class Union : Operation
             this.operand1 = operand1;
             this.operand2 = operand2;
         }
-
-        public override bool IsInside(ref double y)
+        public Difference(ref Circle operand1, ref Circle operand2) : base(ref operand1, ref operand2)
         {
-
-            bool insideOperand1 = operand1.IsInside(ref y);
-            bool outsideOperand2 = !operand2.IsInside(ref y);
-            return insideOperand1 || outsideOperand2;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
         }
         public override bool IsInside(ref double x, ref double y)
         {
             bool insideOperand1 = operand1.IsInside(ref x, ref y);
             bool outsideOperand2 = operand2.IsInside(ref x, ref y);
-            return insideOperand1 | outsideOperand2;
+            return insideOperand1 && !outsideOperand2;
         }
     }
     public class Intersection : Operation
@@ -236,14 +192,12 @@ public class Union : Operation
             this.operand1 = operand1;
             this.operand2 = operand2;
         }
-
-        public override bool IsInside(ref double y)
+        public Intersection(ref Rectangle operand1, ref Difference operand2) : base(ref operand1, ref operand2)
         {
-
-            bool insideOperand1 = operand1.IsInside(ref y);
-            bool insideOperand2 = operand2.IsInside(ref y);
-            return insideOperand1 && insideOperand2;
+            this.operand1 = operand1;
+            this.operand2 = operand2;
         }
+
         public override bool IsInside(ref double x, ref double y)
         {
             bool insideOperand1 = operand1.IsInside(ref x, ref y);
--- Lecture 2/Assignment_1/Assignment_1/Program.cs
+++ Lecture 2/Assignment_1/Assignment_1/Program.cs
@@ -11,51 +11,37 @@ namespace ProSE_Assignment1
 {
     class Program
     {
-        // public const int sizeGridX = 5;
-        // public const int sizeGridY = 5;
-        // public const char gridChar= '.';
         public static void Main(string[] args)
         {
             //Assignment 1 Solution
-            /*Rectangle rec1 = new Rectangle(0, 50, 50, 60);
-            Rectangle rec2 = new Rectangle(20, 30, 1, 50);
-
-            Rectangle rec3 = new Rectangle(50, 60, 0, 60);
-            Rectangle rec4 = new Rectangle(60, 100, 0, 5);
-
-            Rectangle rec5 = new Rectangle(90, 100, 0, 60);
-            Rectangle rec6 = new Rectangle(90, 180, 50, 60);
-
-            Rectangle rec7 = new Rectangle(128, 138, 0, 60);
-            Rectangle rec8 = new Rectangle(170, 180, 0, 60);
-
-            Union newunion = new Union(ref rec1, ref rec2);
-            Union newunion2 = new Union(ref rec3, ref rec4);
-            Union newunion3 = new Union(ref rec5, ref rec6);
-            Union newunion4 = new Union(ref rec7, ref rec8);
-            Union hepsi = new Union(ref newunion, ref newunion2);
-            Union hepsi2 = new Union(ref hepsi, ref newunion3);
-            Union hepsi3 = new Union(ref hepsi2, ref newunion4);
-
-            hepsi3.Visualize(200, 60);*/
-            //Rectangle rec1 = new Rectangle(0,50,0,60);
-            //Rectangle rec2 = new Rectangle(20, 30, 1, 50);
-            //Difference newdifference = new Difference(ref rec1, ref rec2);
-            //newdifference.Visualize(100, 100);
 
 
-            Circle cir1 = new Circle(20, 20, 5);
-            cir1.Visualize(50, 50);
-            //rec1.Visualize(50, 30);
+            // Letter T
+            Rectangle recT1 = new Rectangle(0, 50, 50, 60);
+            Rectangle recT2 = new Rectangle(20, 30, 1, 50);
+            Union letterT = new Union(ref recT1, ref recT2);
 
 
-            //rec2.Visualize(50,40);
+            // Letter U
+            Rectangle recU1 = new Rectangle(50, 100, 0, 60);
+            Rectangle recU2 = new Rectangle(60, 90, 10, 60);
+            Difference letterU = new Difference(ref recU1, ref recU2);
 
+            // Letter M
+            Rectangle recM1 = new Rectangle(90, 100, 0, 60);
+            Rectangle recM2 = new Rectangle(100, 170, 0, 60);
+            Rectangle recM3 = new Rectangle(170, 180, 0, 60);
+            Circle circM1 = new Circle(135, 60, 35);
+            Circle circM2 = new Circle(135, 60, 25);
+            Difference ringM1 = new Difference(ref circM1, ref circM2);
+            Intersection interM1 = new Intersection(ref recM2, ref ringM1);
+            Union letterM1 = new Union(ref recM1, ref recM3);
+            Union letterM = new Union(ref letterM1, ref ringM1);
 
-
-
-
-
+            // Combination of Letters
+            Union lettersTU = new Union(ref letterT, ref letterU);
+            Union logoTUM = new Union(ref lettersTU, ref letterM);
+            logoTUM.Visualize(200, 60);
 
         }
 
