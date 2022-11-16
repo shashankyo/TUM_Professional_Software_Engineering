f0f30f3fae14233613d5c94a80897746c77542d9
ge85git <ge85git@mytum.de>
2022-11-16 23:15:41 +0100

    Lecture 3 Exercises

---
+++ Lecture 3/Lecture_3_Exercises/Lecture_3_Exercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProSE_Lecture3
+{
+    public class MyFirstClass
+    {
+        public static void Main(string[] args)
+        {
+            Vector2D firstvector = new Vector2D(3, 4);
+            firstvector.PrintNorm();
+            Vector3D secondvector = new Vector3D(3, 4, 5);
+            secondvector.PrintNorm();
+        }
+    }
+}
---
+++ Lecture 3/Lecture_3_Exercises/Lecture_3_Exercises/Vector.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProSE_Lecture3
+{
+    public interface IVector
+    {
+    public double GetNorm();
+    public void PrintNorm();
+    }
+    public class Vector2D : IVector
+    {
+        public double x;
+        public double y;
+
+        public Vector2D(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+
+        }
+    public double GetNorm()
+        {
+            double value = Math.Sqrt(x * x + y * y);
+            return value;
+        }
+        public void PrintNorm()
+        {
+            Console.WriteLine(GetNorm());
+        }
+    }
+
+    public class Vector3D : IVector
+    {
+        double x;
+        double y;
+        double z;
+        public Vector3D(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+        public double GetNorm()
+        {
+            double value = Math.Sqrt(x * x + y * y + z * z);
+            return value;
+        }
+        public void PrintNorm()
+        {
+            Console.WriteLine(GetNorm());
+        }
+
+    }
+    
+}
