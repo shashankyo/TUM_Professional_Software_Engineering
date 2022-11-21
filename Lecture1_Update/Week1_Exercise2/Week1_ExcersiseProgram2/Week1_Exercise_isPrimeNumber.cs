bb77359eca3ce01e1966c447e4eba472e776e503
korayinal <korayinal93@gmail.com>
2022-11-21 19:19:56 +0100

    Rakuncuk <3

---
+++ Lecture1_Update/Week1_Exercise2/Week1_ExcersiseProgram2/Week1_Exercise_isPrimeNumber.cs
@@ -0,0 +1,43 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+    namespace ProSE
+    {
+        class MyFirstClass
+        {
+            static void Main(string[] args)
+            {
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int i = 2;
+            bool isPrime = true;
+            do
+            {
+                if (number % i == 0)
+                {
+                    //Console.WriteLine("Burda");
+                    isPrime = false;
+                    Console.WriteLine(number + " is not a prime number.");
+                    //break;
+
+                }
+                else
+                {
+                    Console.WriteLine(number + " is a prime number.");
+                }
+
+                i++;
+            }
+            while (i <= number/2);
+
+            if (isPrime)
+            {
+                Console.WriteLine(number + " is a prime number.");
+            }
+            else
+            {
+                Console.WriteLine(number + " is not a prime number.");
+            }
+        }
+
+        }
+    }
---
+++ Lecture1_Update/Week1_ExtraExercises/ifSortedAscending/ifSortedAscending/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+    namespace MyNameSpace
+{
+    class MyFirstClass
+    {
+        public static void Main(string[] args)
+        {
+            int[] inputArray = new int[3];
+            Console.WriteLine("Type in 3 numbers:");
+            inputArray[0] = Convert.ToInt32(Console.ReadLine());
+            inputArray[1] = Convert.ToInt32(Console.ReadLine());
+            inputArray[2] = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Input Array is: " + string.Join(",", inputArray));
+            bool isSorted = true;
+            for (int i=0; i <inputArray.Length -1; i++)
+            {
+                if (inputArray[i] > inputArray[i+1])
+                {
+                    isSorted = false;
+                }
+            }
+        
+            if (isSorted == true)
+            {
+                Console.WriteLine((string.Join(",", inputArray) + " given order of numbers are in an ascending order."));
+
+            }
+            else
+            {
+                Console.WriteLine((string.Join(",", inputArray) + " given order of numbers are not in an ascending order."));
+            }
+            
+
+
+        }
+    }
+}
