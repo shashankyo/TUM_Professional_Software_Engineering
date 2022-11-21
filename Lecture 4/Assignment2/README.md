95f26e2c492bb0ed6b4ef0e6156c073f27dbe739
korayinal <korayinal93@gmail.com>
2022-11-21 15:59:05 +0100

    Assignment 2 Start

---
+++ Lecture 4/Assignment2/README.md
@@ -0,0 +1,11 @@
+# Assignment-2
+
+Hello,
+
+Make sure to submit your solution as a zipped folder to moodle.
+
+The zipped folder should be named as "matriculation_number_assignment-2.zip". 
+
+Best, 
+
+ProSE's lecturers and tutors
---
+++ Lecture 4/Assignment2/SimpleShop/DataTypes.cs
@@ -0,0 +1,50 @@
+/* ------------------- DO NOT EDIT ----------------------
+ * This file contains custom data-types like enums and structs used in this project
+ * You are not required to edit this. */
+
+namespace SimpleShop{
+    public enum KeywordTypes{
+            String,
+            Int,
+            Decimal
+        } // a custom enumerator type
+    
+    public struct Keyword{
+        // local variables
+        private string keyword;
+        private KeywordTypes type;
+
+        // constructor
+        public Keyword(string keyword, KeywordTypes Type = KeywordTypes.String){
+            this.keyword = keyword;
+            this.type = Type;
+        }
+
+        // custom Struct methods
+        public string GetStart(){
+            return "<" + this.keyword + ">";
+        }
+
+        public string GetEnd(){
+            return "</" + this.keyword + ">";
+        }
+
+        public string GetString(){
+            return this.keyword;
+        }
+
+        public KeywordTypes WhichType(){
+            return this.type;
+        }
+    }
+
+    public struct KeywordPair{
+        public Keyword Key;
+        public string Value;
+
+        public KeywordPair(Keyword key, string value){
+            this.Key = key;
+            this.Value = value;
+        }
+    }
+}
