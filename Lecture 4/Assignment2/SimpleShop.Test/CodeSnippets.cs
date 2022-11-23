8addccf1c37cfe543d15434b5c5e55fa617ad2ef
korayinal <korayinal93@gmail.com>
2022-11-24 00:49:44 +0100

    Correction_FileNaming

--- Lecture 4/Assignment2/SimpleShop.Test/CodeSnippets.cs
+++ Lecture 4/Assignment2/SimpleShop.Test/CodeSnippets.cs
@@ -5,11 +5,7 @@ namespace CodeSnippets{
     public class CodeSnippets{
 
         /// <summary>
-        /// converts the given string into an array of characters
-        /// reverses the order of the array and converts all characters to lowercase
-        /// checks if the reversed array(all characters converted to lowercase) is the same with given string(everything converted to lowercase)
-        /// returns the result as bool
-        /// short summary: checks if the given string and its reversed version are the same, returns the result as bool
+        /// checks if the given string and its reversed version are the same, returns the result as bool
         /// </summary>
         static bool function1(string pattern) {
             var parts = pattern.ToCharArray();
@@ -22,8 +18,7 @@ namespace CodeSnippets{
         
         
         /// <summary>
-        /// recieves an array of integers as arguement
-        /// 
+        /// sorts a given array of integers in ascending order
         /// </summary>
         public static int function2(int[] numbers){
             for (var h = numbers.Length / 2; h > 0; h /= 2)
