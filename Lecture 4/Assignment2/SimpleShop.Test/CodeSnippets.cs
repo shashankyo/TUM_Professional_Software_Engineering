a3f869116260971691075e0d1cda1599a0e7be2e
korayinal <korayinal93@gmail.com>
2022-11-24 00:28:56 +0100

    Assignment 2 Start

--- Lecture 4/Assignment2/SimpleShop.Test/CodeSnippets.cs
+++ Lecture 4/Assignment2/SimpleShop.Test/CodeSnippets.cs
@@ -3,9 +3,13 @@ using System;
 namespace CodeSnippets{
     
     public class CodeSnippets{
-        
+
         /// <summary>
-        /// Describe the function here
+        /// converts the given string into an array of characters
+        /// reverses the order of the array and converts all characters to lowercase
+        /// checks if the reversed array(all characters converted to lowercase) is the same with given string(everything converted to lowercase)
+        /// returns the result as bool
+        /// short summary: checks if the given string and its reversed version are the same, returns the result as bool
         /// </summary>
         static bool function1(string pattern) {
             var parts = pattern.ToCharArray();
@@ -18,14 +22,18 @@ namespace CodeSnippets{
         
         
         /// <summary>
-        /// Describe the function here.
+        /// recieves an array of integers as arguement
+        /// 
         /// </summary>
         public static int function2(int[] numbers){
-            for (var h = numbers.Length / 2; h > 0; h /= 2){
-                for (var i = h; i < numbers.Length; i += 1){
+            for (var h = numbers.Length / 2; h > 0; h /= 2)
+            {
+                for (var i = h; i < numbers.Length; i += 1)
+                {
                     var temp = numbers[i];
                     int t;
-                    for (t = i; t >= h && numbers[t - h] > temp; t -= h){
+                    for (t = i; t >= h && numbers[t - h] > temp; t -= h)
+                    {
                         numbers[t] = numbers[t - h];
                     }
                     numbers[t] = temp;
