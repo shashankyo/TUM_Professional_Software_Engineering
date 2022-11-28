6c264460c499e224a5cb56e700280c9ad7685440
korayinal <korayinal93@gmail.com>
2022-11-28 17:18:27 +0100

    Lecture 5 - Exercise - Schedule App Started

---
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/DataTypes.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProSE
+{
+    public enum Days
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+    }
+    public class Lecture
+    {
+        public string startTime; 
+        public string endTime;
+        public string lectureName;
+        public string lectureDay;
+        
+    }
+    public class Timetable
+    {
+        public Lecture timetableLecture;
+        public string owner;
+
+        public Timetable(Lecture timetableLecture, string owner)
+        {
+            this.timetableLecture = timetableLecture;
+            this.owner = owner;
+        }
+
+
+
+
+    }
+}
---
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Xml;
+
+namespace ProSE
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Koray Program");
+
+            // File Path
+            string filePath = "D:\\WIP\\00_Akademic_TUM_ITBE\\1. Semester\\Professional Software Engineering\\Lecture 5\\Lecture 5 - Schedule App\\Lecture 5 - Schedule App\\Koray_Inal_Terminkalender_Woche.xml";
+            
+            XmlTextReader reader = new XmlTextReader(filePath);
+            
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+            }
+
+        }
+    }
+}
