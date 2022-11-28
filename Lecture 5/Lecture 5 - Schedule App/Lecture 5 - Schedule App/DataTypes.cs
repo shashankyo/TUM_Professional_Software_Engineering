26b21fe3b6f63aba13d55ca46b741209fe6bed03
korayinal <korayinal93@gmail.com>
2022-11-28 22:58:50 +0100

    Lecture5_Schedule App_working..

--- Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/DataTypes.cs
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/DataTypes.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ProSE
 {
@@ -19,22 +20,57 @@ namespace ProSE
         public string startTime; 
         public string endTime;
         public string lectureName;
-        public string lectureDay;
+        public Days lectureDay; 
         
-    }
-    public class Timetable
-    {
-        public Lecture timetableLecture;
-        public string owner;
-
-        public Timetable(Lecture timetableLecture, string owner)
+        private Lecture(string startTime, string endTime, string lectureName, Days lectureDay)
         {
-            this.timetableLecture = timetableLecture;
-            this.owner = owner;
+            this.startTime = startTime;
+            this.endTime = endTime;
+            this.lectureName = lectureName;
+            this.lectureDay = lectureDay;
+        }
+        public static Lecture[] GetLectures(string filePath)
+        {
+            XmlTextReader reader = new XmlTextReader(filePath);
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element: // The node is an element.
+                        Console.Write("<" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+
+                    case XmlNodeType.Text: //Display the text in each element.
+                        Console.WriteLine(reader.Value);
+                        break;
+
+                    case XmlNodeType.EndElement: //Display the end of the element.
+                        Console.Write("</" + reader.Name);
+                        Console.WriteLine(">");
+                        break;
+                }
+                
+            }
+            Lecture[] lectures = new Lecture();
+            return lectures;
+        }
+        public string GetRows()
+        {
+            switch();
+        }
+    }
+        public class Timetable
+        {
+            public Lecture timetableLecture;
+            public string owner;
+
+            public Timetable(Lecture timetableLecture, string owner)
+            {
+                this.timetableLecture = timetableLecture;
+                this.owner = owner;
+            }
         }
 
 
-
-
-    }
 }
--- Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/Program.cs
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/Program.cs
@@ -11,28 +11,9 @@ namespace ProSE
 
             // File Path
             string filePath = "D:\\WIP\\00_Akademic_TUM_ITBE\\1. Semester\\Professional Software Engineering\\Lecture 5\\Lecture 5 - Schedule App\\Lecture 5 - Schedule App\\Koray_Inal_Terminkalender_Woche.xml";
-            
-            XmlTextReader reader = new XmlTextReader(filePath);
-            
-            while (reader.Read())
-            {
-                switch (reader.NodeType)
-                {
-                    case XmlNodeType.Element: // The node is an element.
-                        Console.Write("<" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
 
-                    case XmlNodeType.Text: //Display the text in each element.
-                        Console.WriteLine(reader.Value);
-                        break;
 
-                    case XmlNodeType.EndElement: //Display the end of the element.
-                        Console.Write("</" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
-                }
-            }
+            Lecture.GetLectures(filePath);
 
         }
     }
