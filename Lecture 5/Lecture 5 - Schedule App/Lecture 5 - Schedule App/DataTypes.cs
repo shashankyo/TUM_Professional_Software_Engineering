9cd60efc8903a4510adbb3af1e933b1430591a05
korayinal <korayinal93@gmail.com>
2022-11-29 23:14:15 +0100

    Schedule XML formating assignment

--- Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/DataTypes.cs
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/DataTypes.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace ProSE
 {
@@ -14,70 +16,100 @@ namespace ProSE
         Wednesday,
         Thursday,
         Friday,
+        Saturday,
+        Sunday,
     }
     public class Lecture
     {
         public string startTime { get; set; }
         public string endTime { get; set; }
         public string lectureName { get; set; }
-        public Days lectureDay {get; set; }
-        
+        public Days lectureDay { get; set; }
+
+
         private Lecture(string startTime, string endTime, string lectureName, Days lectureDay)
         {
-            this.startTime = startTime;
-            this.endTime = endTime;
             this.lectureName = lectureName;
             this.lectureDay = lectureDay;
-        }
-        public static Lecture GetLectures(string filePath)
-        {
-            XmlTextReader reader = new XmlTextReader(filePath);
-            
-            while (reader.Read())
-            {
 
-                switch (reader.NodeType)
+            bool isValidStart = DateTime.TryParse(startTime, out _);
+            bool isValidEnd = DateTime.TryParse(endTime, out _);
+
+            if (isValidStart && isValidEnd)
+            {
+                this.startTime = startTime;
+                this.endTime   = endTime;
+            }
+            else
+            {
+                Console.WriteLine("Invalid Time. Setting time from 12:00 to 1:00 am");
+
+                this.startTime = "12:00 AM";
+                this.endTime = "1:00 AM";
+            }
+
+        }
+        public static Lecture GetLectures(XmlNode lecture)
+        {
+            string lectureName = " ", startTime = " ", endTime = " ";
+            Days day = Days.Monday;
+            foreach (XmlNode node in lecture)
+            {
+                if (node.Name == "TITEL")
                 {
-                    case XmlNodeType.Element: // The node is an element.
-                        Console.Write(reader.Name);
-                        Console.Write(reader.GetType());
-                        Console.WriteLine(">");
-                        break;
-
-                    case XmlNodeType.Text: //Display the text in each element.
-                        Console.WriteLine(reader.Value);
-                        break;
-
-                    case XmlNodeType.EndElement: //Display the end of the element.
-                        Console.Write("</" + reader.Name);
-                        Console.WriteLine(">");
-                        break;
-                
-                XmlNode 
-                
+                    lectureName = node.InnerText;
                 }
+                else if (node.Name == "DATUM")
+                {
+                    var deCultureInfo = new CultureInfo("de-DE");
+                    DateTime dateValue = DateTime.ParseExact(node.InnerText, "dd.MM.yyyy", deCultureInfo);
 
-                
+                    bool isValidDay = Enum.TryParse(dateValue.ToString("dddd"), out day);
+                }
+                else if (node.Name == "VON")
+                {
+                    startTime = node.InnerText;
+                }
+                else if (node.Name == "BIS")
+                {
+                    endTime = node.InnerText;
+                }
             }
-            Lecture lectures = new Lecture("x","x0","x",Days.Monday);
-            return lectures;
+            return new Lecture(lectureName,startTime,endTime, day );
         }
-        //public string GetRows()
-        //{
-        //    switch();
-        //}
+        
     }
-        public class Timetable
-        {
-            public Lecture timetableLecture;
-            public string owner;
+    class Timetable
+    {
+        public List<Lecture> Schedule { get; set; } = new List<Lecture>();
 
-            public Timetable(Lecture timetableLecture, string owner)
-            {
-                this.timetableLecture = timetableLecture;
-                this.owner = owner;
-            }
+        public Timetable() { }
+
+        public void PushBack(Lecture lecture)
+        {
+            this.Schedule.Add(lecture);
         }
+
+        public static Timetable SetupFromXML(string filePath)
+        {
+            var scheduleXML = new XmlDocument();
+            scheduleXML.LoadXml(filePath);
+
+            var lectureNodes = scheduleXML.DocumentElement.ChildNodes;
+
+            var timetable = new Timetable();
+
+            foreach (XmlNode lectureNode in lectureNodes)
+            {
+                var lecture = Lecture.GetLectures(lectureNode);
+
+                timetable.PushBack(lecture);
+            }
+
+            return timetable;
+        }
+    }
+
 
 
 }
--- Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/Program.cs
+++ Lecture 5/Lecture 5 - Schedule App/Lecture 5 - Schedule App/Program.cs
@@ -12,7 +12,7 @@ namespace ProSE
             // File Path
             string filePath = "D:\\WIP\\00_Akademic_TUM_ITBE\\1. Semester\\Professional Software Engineering\\Lecture 5\\Lecture 5 - Schedule App\\Lecture 5 - Schedule App\\Koray_Inal_Terminkalender_Woche.xml";
 
-
+            Lecture.Set(filePath);
             Lecture.GetLectures(filePath);
 
         }
