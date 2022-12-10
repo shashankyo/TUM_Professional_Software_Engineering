ff356694cfec9c1ce5afb44b7f1eb82262ba5819
korayinal <korayinal93@gmail.com>
2022-12-10 23:49:53 +0100

    Assignment 3 and Lecture 7 Exercises

---
+++ Lecture 7/Lecture7-Exercises/Lecture7-Exercises/Delegates.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static ProSE.Delegates;
+
+namespace ProSE
+{
+
+    class Delegates
+    {
+        public delegate int AdditionHandler();
+        static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            AdditionHandler op = new AdditionHandler(Add);
+            Console.WriteLine(op(10, 5));
+            Console.ReadLine();
+
+        }
+        //public delegate Type AdditionHandler();
+
+        //public  Type Add(Type firstArg, Type secondArg)
+        //{
+
+        //    return firstArg + secondArg;
+
+        //}
+
+
+
+
+        //AdditionHandler additionHandlerFirst = new AdditionHandler(Add);
+        //AdditionHandler additionHandlerSecond = new AdditionHandler(Add);
+        //AdditionHandler additionHandlerThird = new AdditionHandler(Add);
+
+
+
+
+    }
+
+
+}
---
+++ Lecture 7/Lecture7-Exercises/Lecture7-Exercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ProSE
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Main Test");
+        }
+    }
+
+}
